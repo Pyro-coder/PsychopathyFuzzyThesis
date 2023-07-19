25cebd7394edad5238a55c984bf52585a9f4a2f0
Noah <62971189+Pyro-coder@users.noreply.github.com>
2023-07-18 21:13:27 -0500

    Added tolerance calculation to algorithms

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy
 from scipy.stats import nct
 import scipy.optimize as opt
+import scipy.stats as stats
 
 # here are the functions
 
@@ -12,50 +13,76 @@ def rgamma(n):
     return 1/fval
 
 
+
 # calculate the cdf of a non-central t statistic
 # t is the cdf variable; n is # degrees of freedom; δ is the noncentrality parameter
-def nct_cdf(t, n, lower_delta):
-    return nct.cdf(t, n, lower_delta)
+def nct_cdf(t, n, delta, tol):
+    r = lambda t_val, n_val: t_val*t_val/(t_val*t_val+n_val)
+    if (t >= 0):
+        out = stats.norm.cdf(-delta)
+        for m in range(0, 1000):
+            if (m <= 50):
+                sumincr = 0.5 * stats.poisson.pmf(m, delta * delta / 2) * (stats.beta.cdf(r(t, n), m + 0.5, n / 2) + ((delta * scipy.special.gamma(m + 1)) / (np.sqrt(2) * scipy.special.gamma(m + 1.5))) * stats.beta.cdf(r(t, n), m + 1, n / 2))
+            else:
+                sumincr = 0.5 * stats.poisson.pmf(m, delta * delta / 2) * (stats.beta.cdf(r(t, n), m + 0.5, n / 2) + (delta / np.sqrt(2) * rgamma(m)) * stats.beta.cdf(r(t, n), m + 1, n / 2))
+            out = out + sumincr
+
+            if (sumincr / out < tol):
+                break
+    else:
+        out = stats.norm.cdf(delta)
+        for m in range(0, 1000):
+            if (m <= 50):
+                sumincr = 0.5 * stats.poisson.pmf(m, delta * delta / 2) * (stats.beta.cdf(r(t, n), m + 0.5, n / 2) + ((delta * scipy.special.gamma(m + 1)) / (np.sqrt(2) * scipy.special.gamma(m + 1.5))) * stats.beta.cdf(r(t, n), m + 1, n / 2))
+            else:
+                sumincr = 0.5 * stats.poisson.pmf(m, delta * delta / 2) * (stats.beta.cdf(r(t, n), m + 0.5, n / 2) + (delta / np.sqrt(2) * rgamma(m)) * stats.beta.cdf(r(t, n), m + 1, n / 2))
+
+            out = out + sumincr
+            if (abs(sumincr)/out < tol):
+                out = 1 - out
+                break
+    return out
 
 
 
+print(nct_cdf(2, 100, 2, 0.00001))
 
 
-def inv_nct_cdf(P, n, delta):
+def inv_nct_cdf(P, n, delta, tol):
     #Inverse of non-central t-statistic cdf; finds the threshold yielding a cdf value of P
     #n is the # of degrees of freedom
 
-    # set initial t guess to lowercase delta (lower_delta) and step up and down to find an interval where f(t) has opposite signs for the endpoints
-    def f(t):
-        return nct.cdf(t, n, delta) - P
+    # set initial t guess to lowercase delta (delta) and step up and down to find an interval where f(t) has opposite signs for the endpoints
+    f = lambda t: nct_cdf(t, n, delta, tol) - P
 
-    # Initialize t to the quantile of the standard t-distribution
-    t = scipy.stats.t.ppf(P, n)
+    x0 = delta
 
     # Determine the direction to move in
-    if f(t) < 0:
-        # If the initial value is less than the target, move up
-        t_upper = t
-        while f(t_upper) < 0:
-            t_upper *= 2
-        t_lower = t_upper / 2
+    if f(x0) <= 0:
+        x1 = 2 * delta
+        while (f(x0) <= 0 and f(x1) <= 0):
+            x0 = x1
+            x1 = x1 + delta
     else:
-        # If the initial value is more than the target, move down
-        t_lower = t
-        while f(t_lower) > 0:
-            t_lower /= 2
-        t_upper = t_lower * 2
-
-    # Once we have a bracket, use a root-finding algorithm to find the t value
-    t_zero = opt.brentq(f, t_lower, t_upper)
+        x0 = 0
+        x1 = delta
+        while (f(x0) > 0 and f(x1) >= 0):
+            x1 = x0
+            x0 = x0 - delta
     
-    return t_zero
 
+    return opt.brentq(f, x0, x1)
+
+
+print(inv_nct_cdf(.95, 50, 2, 0.000001))
     
-def ktol(upsilon, a, n):
-    zp = scipy.stats.norm.ppf(1 - upsilon, 0, 1)
-    lower_delta = zp * np.sqrt(n)
-    result = inv_nct_cdf(1-a, n-1, lower_delta)/np.sqrt(n)
+import math
+
+def ktol(upsilon, a, n, tol):
+    #Find one-sided tolerance interval k (multiple of sample std dev away from sample mean), given γ, α and n
+    zp = stats.norm.ppf(1-upsilon, 0, 1)
+    delta = math.sqrt(n) * zp
+    result = inv_nct_cdf(1 - a, n - 1, delta, tol) / math.sqrt(n)
     return result
 
-print (ktol(.05, .05, 175))
+print (ktol(.05, .05, 175, 0.000001))
