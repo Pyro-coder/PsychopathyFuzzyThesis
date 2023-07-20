1e353a52a11c407c1181f5452c73a35bcdc6676c
Noah <62971189+Pyro-coder@users.noreply.github.com>
2023-07-19 21:53:41 -0500

    to do: test newest function

--- noncenteral_tstatic_cdf.py
+++ noncenteral_tstatic_cdf.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def trap(x, ht, lb, lt, rt, rb):
     #trapezoid of height ht, left base lb, left top lt, right top rt, right base rb
     if (x < lb):
@@ -11,3 +14,18 @@ def trap(x, ht, lb, lt, rt, rb):
     if (rt < x <= rb):
         tr = ht * (rb - x) / (rb - rt)
     return tr
+
+def bad_data(x, x0, x1):
+    #eliminates invalid intervals
+    y = None
+    a = x[0]
+    b = x[1]
+    for i in range(0, len(x)):
+        #remove rows with infeasable endpoints
+        if ((x < a[i] < b[i] < x1) and (b[i] - a[i] < x1 - x0)):
+            if y is None:
+                y = [a[i], b[i]]
+            else:
+                y = np.vstack((y, [a[i], b[i]]))
+    return y
+
