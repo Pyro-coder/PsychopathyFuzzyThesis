b1f1b855d71c109727fc5978af181ac469ad0c10
Noah <oppermann.noah@gmail.com>
2023-07-22 11:17:37 -0500

    got it somewhat working

--- noncenteral_tstatic_cdf.py
+++ noncenteral_tstatic_cdf.py
@@ -1,40 +1,41 @@
 import numpy as np
 import scipy
-import math
 
+from scipy.integrate import quad
 from scipy.optimize import root
-from scipy.stats import chi2
+from scipy.stats import chi2, norm
 
 import openpyxl
 
 
 def trap(x, ht, lb, lt, rt, rb):
-    #trapezoid of height ht, left base lb, left top lt, right top rt, right base rb
-    if (x < lb):
+    # trapezoid of height ht, left base lb, left top lt, right top rt, right base rb
+    if x < lb:
         return 0
-    if (x > rb):
+    if x > rb:
         return 0
-    if (lt <= x <= rt):
+    if lt <= x <= rt:
         return ht
-    if (lb <= x < lt):
+    if lb <= x < lt:
         tr = ht * (x - lb) / (lt - lb)
-    if (rt < x <= rb):
+    if rt < x <= rb:
         tr = ht * (rb - x) / (rb - rt)
     return tr
 
 
 def bad_data(x, x0, x1):
-    #eliminates invalid intervals
+    # eliminates invalid intervals
     y = None
     for i in range(0, len(x)):
         a, b = x[i]
-        #remove rows with infeasible endpoints
+        # remove rows with infeasible endpoints
         if ((x0 < a < b < x1) and (b - a < x1 - x0)):
             if y is None:
                 y = [[a, b]]
             else:
                 y.append([a, b])
     return y
+
 
 data = [[0, 10],
         [-1, 10],
@@ -44,10 +45,7 @@ data = [[0, 10],
         [3, 3],
         [50, 10]]
 
-
 print(bad_data(data, 0, 10))
-
-
 
 web_data_copy = openpyxl.load_workbook('excel\WEBdatacopy.xlsx')
 
@@ -55,22 +53,23 @@ wdc_sheet = web_data_copy['data']
 
 range_values = wdc_sheet['AI2:AJ175']
 
-#for row in range_values:
+
+# for row in range_values:
 #    for cell in row:
 #        print(cell.value, end=',')
 #    print()
 
 def box_and_whisker(x):
-    #calculate quartiles and inter-quartile ranges on right or left interval endpoints
-    #x is an n-vector of right or left interval endpoints
-    #calculate quartile index
+    # calculate quartiles and inter-quartile ranges on right or left interval endpoints
+    # x is an n-vector of right or left interval endpoints
+    # calculate quartile index
     nq = np.floor(len(x) / 4)
 
-    #skip this test if nq = 0
+    # skip this test if nq = 0
     if (nq == 0):
         return x
-    
-    #find first and third quartile values
+
+    # find first and third quartile values
     remainder = np.mod(len(x), 4)
     y = np.sort(x)
     if (remainder == 0):
@@ -86,32 +85,33 @@ def box_and_whisker(x):
         q25 = y[int(nq)]
         q75 = y[int(3 * nq + 2)]
 
-    #find inner quartile range and bounds of valid endpoints
+    # find inner quartile range and bounds of valid endpoints
     iqr = q75 - q25
     xmin = q25 - 1.5 * iqr
     xmax = q75 + 1.5 * iqr
     return [xmin, xmax]
 
+
 def outlier_test(x):
-    #flags outlier endpoints
-    #x is an n x 2 vector of right and left interval endpoints
-    #calculate box and whisker test on x[0] and x[1]
+    # flags outlier endpoints
+    # x is an n x 2 vector of right and left interval endpoints
+    # calculate box and whisker test on x[0] and x[1]
     x = np.array(x)
-    x0 = box_and_whisker(x[:,0])
-    x1 = box_and_whisker(x[:,1])
+    x0 = box_and_whisker(x[:, 0])
+    x1 = box_and_whisker(x[:, 1])
     y = []
 
-    #eliminate outlier endpoints in x using box and whisker test
+    # eliminate outlier endpoints in x using box and whisker test
     for i in range(0, len(x)):
-        if (x0[0] <= x[i,0] <= x0[1] and x1[0] <= x[i,1] <= x1[1]):
+        if (x0[0] <= x[i, 0] <= x0[1] and x1[0] <= x[i, 1] <= x1[1]):
             y.append(x[i])
     if len(y) == 0:
         return None
-    
+
     y = np.array(y)
-    #y has first pass outliers eliminated
-    #repeat box and whisker test on L=y[1]-y[0]
-    L = y[:,1] - y[:,0]
+    # y has first pass outliers eliminated
+    # repeat box and whisker test on L=y[1]-y[0]
+    L = y[:, 1] - y[:, 0]
     Lbw = box_and_whisker(L)
     z = []
     for i in range(0, len(y)):
@@ -130,31 +130,33 @@ outlietest = [[0, 6],
               [5, 5.5],
               [5.2, 5.3]]
 
-#print(outlier_test(outlietest))
+
+# print(outlier_test(outlietest))
 
 
 def fr(y, a):
     # root function for Eq. A. 18 of reference [2]
     r = y
+
     def f(x):
-        return scipy.stats.norm.cdf(y + r) - scipy.stats.norm.cdf(y - r) - (1 - a)
+        return norm.cdf(y + r) - norm.cdf(y - r) - (1 - a)
+
     z = root(f, r)
     return z.x[0]
 
 def fk(xk, a, n):
     # Q(lambda, k) Eq. A. 17 of reference [2]
-    integrand = lambda y: 1 - chi2.cdf(((n - 1) * fr(y, a)**2) / xk**2, n - 1) * np.exp(((-1) / 2) * n * y * y)
-    print("About to integrate integrand = ", integrand)
-    output, _ = scipy.integrate.quad(integrand, 0, np.inf)
-    print("integration output = ", output)
+    def integrand(y):
+        return 1 - chi2.cdf(((n - 1) * fr(y, a) ** 2) / xk ** 2, n - 1) * np.exp(((-1) / 2) * n * y * y)
+
+    output = quad(integrand, 0, np.inf)[0]
     return (2 * np.sqrt(n)) / (np.sqrt(2 * np.pi)) * output
 
 def kk(a, upsilon, n):
     # solve for k of Eq. A. 14 of reference [2]
     xk = 2
-    def f(xk):
-        return fk(xk, a, n) - (1 - upsilon)
-    z = root(f, xk)
+
+    z = root(lambda x: fk(x, a, n) - (1 - upsilon), xk)
     return z.x[0]
 
 print(kk(0.05, 0.05, 30))
